ebb2f34a89fa12573dab11ecad92f5ecdd9de79f
Idrissa KONKOBO <ikonkobo.idrissa@gmail.com>
2022-11-25 15:12:03 +0100

    chatbot + consumption init

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,9 +1,10 @@
-from actions.database_connector import DataUpdate
 from typing import Any, Text, Dict, List
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
-# from rasa_sdk.events import EventType
+
+from actions.database_connector import DataUpdate
+from actions.testing_slots import email_isValid, zipcode_isValid, civility_isValid, birthyear_isValid, ecoloscore_isValid, workdayoccupation_isValid, consprofile_isValid, maxpower_isValid
 
 
 class PersonalInfosFormValidation(FormValidationAction):
@@ -11,23 +12,23 @@ class PersonalInfosFormValidation(FormValidationAction):
     def name(self) -> Text:
         return "validate_personal_data_form"
 
-    def validate_lastname(
+    def validate_civility(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
-        """Validate last name value."""
+        """Validate civility value."""
 
-        if len(value) > 1:
-            # validation succeeded, set the value of the "size" slot to value
-            return {"lastname": value}
+        if civility_isValid(value):
+            # validation succeeded, set the value of the "civility" slot to value
+            return {"civility": value}
         else:
-            dispatcher.utter_message(response="utter_wrong_lastname")
+            dispatcher.utter_message(response="utter_wrong_civility")
             # validation failed, set this slot to None, meaning the
             # user will be asked for the slot again
-            return {"lastname": None}
+            return {"civility": None}
 
     def validate_firstname(
         self,
@@ -39,7 +40,7 @@ class PersonalInfosFormValidation(FormValidationAction):
         """Validate first name value."""
 
         if len(value) > 1:
-            # validation succeeded, set the value of the "size" slot to value
+            # validation succeeded, set the value of the "firstname" slot to value
             return {"firstname": value}
         else:
             dispatcher.utter_message(response="utter_wrong_firstname")
@@ -47,23 +48,23 @@ class PersonalInfosFormValidation(FormValidationAction):
             # user will be asked for the slot again
             return {"firstname": None}
 
-    def validate_email(
+    def validate_lastname(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
-        """Validate email value."""
+        """Validate last name value."""
 
         if len(value) > 1:
-            # validation succeeded, set the value of the "size" slot to value
-            return {"email": value}
+            # validation succeeded, set the value of the "lastname" slot to value
+            return {"lastname": value}
         else:
-            dispatcher.utter_message(response="utter_wrong_email")
+            dispatcher.utter_message(response="utter_wrong_lastname")
             # validation failed, set this slot to None, meaning the
             # user will be asked for the slot again
-            return {"email": None}
+            return {"lastname": None}
 
     def validate_zipcode(
         self,
@@ -74,14 +75,122 @@ class PersonalInfosFormValidation(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate zipcode value."""
 
-        if len(value) == 5:
-            # validation succeeded, set the value of the "size" slot to value
+        if zipcode_isValid(value):
+            # validation succeeded, set the value of the "zipcode" slot to value
             return {"zipcode": value}
         else:
             dispatcher.utter_message(response="utter_wrong_zipcode")
             # validation failed, set this slot to None, meaning the
             # user will be asked for the slot again
             return {"zipcode": None}
+
+    def validate_email(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate email value."""
+
+        if email_isValid(value):
+            # validation succeeded, set the value of the "birthyear" slot to value
+            return {"email": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_email")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"email": None}
+
+    def validate_birthyear(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate birthyear value."""
+
+        if birthyear_isValid(value):
+            # validation succeeded, set the value of the "birthyear" slot to value
+            return {"birthyear": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_birthyear")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"birthyear": None}
+
+    def validate_ecoloscore(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate ecoloscore value."""
+
+        if ecoloscore_isValid(value):
+            # validation succeeded, set the value of the "ecoloscore" slot to value
+            return {"ecoloscore": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_ecoloscore")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"ecoloscore": None}
+
+    def validate_workdayoccupation(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate workdayoccupation value."""
+
+        if workdayoccupation_isValid(value):
+            # validation succeeded, set the value of the "workdayoccupation" slot to value
+            return {"workdayoccupation": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_workdayoccupation")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"workdayoccupation": None}
+
+    def validate_maxpower(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate maxpower value."""
+
+        if maxpower_isValid(value):
+            # validation succeeded, set the value of the "maxpower" slot to value
+            return {"maxpower": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_maxpower")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"maxpower": None}
+
+    def validate_consprofile(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate consprofile value."""
+
+        if consprofile_isValid(value):
+            # validation succeeded, set the value of the "consprofile" slot to value
+            return {"consprofile": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_consprofile")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"consprofile": None}
 
 
 class ActionSubmitForm(Action):
@@ -97,8 +206,11 @@ class ActionSubmitForm(Action):
      ) -> List[Dict]:
 
         DataUpdate(
-            tracker.get_slot("lastname"), tracker.get_slot("firstname"),
-            tracker.get_slot("email"), tracker.get_slot("zipcode"))
+            tracker.get_slot("civility"), tracker.get_slot("lastname"), 
+            tracker.get_slot("firstname"), tracker.get_slot("zipcode"),
+            tracker.get_slot("email"), tracker.get_slot("birthyear"),
+            tracker.get_slot("ecoloscore"), tracker.get_slot("workdayoccupation"), 
+            tracker.get_slot("maxpower"), tracker.get_slot("consprofile"))
 
         dispatcher.utter_message(template="utter_submit")
         return []
@@ -108,25 +220,3 @@ class ActionSubmitForm(Action):
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
--- chatbot/actions/database_connector.py
+++ chatbot/actions/database_connector.py
@@ -1,28 +1,36 @@
 import psycopg2
-from typing import Any, Text, Dict, List
+from typing import Text
 import yaml
 from yaml.loader import SafeLoader
 
 
 def DataUpdate(
+        civility_: Text,
         last_name_: Text, 
         first_name_: Text, 
         email_: Text, 
-        zip_code_: Text) -> None:
+        zip_code_: Text,
+        birthyear_: Text,
+        ecolo_score_: Text,
+        workday_occupation_: Text, 
+        max_power_: Text,
+        cons_profile_: Text) -> None:
     '''
     Pushes Descriptive Analytics Data to the Database
     '''
     # Open the file and load the file
-    with open('config_db.yml') as f:
+    with open('C:/Users/idris/Desktop/ENSAE/S1_3A/Cloud_Computing/EnergyBot/chatbot/config_db.yml') as f:
         credentials = yaml.load(f, Loader=SafeLoader)
-    db = psycopg2.connect(
+    db_connexion = psycopg2.connect(
         database=credentials["database"],
         host=credentials["host"],
         user=credentials["user"],
         password=credentials["password"],
         port=credentials["port"])
-    mycursor = db.cursor()
-    query = f"INSERT INTO customer(last_name, first_name, email, zip_code) \
-    VALUES ('{last_name_}', '{first_name_}', '{email_}', '{zip_code_}')"
+    mycursor = db_connexion.cursor()
+    query = f"""INSERT INTO customer_data("CIVILITY", "FIRSTNAME", "LASTNAME", \
+    "ZIPCODE", "EMAIL", "BIRTHYEAR", "ECOLO_SCORE", "WORKDAY_OCCUPATION", "MAX_POWER", "CONS_PROFILE") \
+    VALUES ('{civility_}', '{first_name_}', '{last_name_}', '{zip_code_}', '{email_}', \
+    {birthyear_}, {ecolo_score_}, '{workday_occupation_}', {max_power_}, '{cons_profile_}')"""
     mycursor.execute(query)
-    db.commit()
+    db_connexion.commit()
